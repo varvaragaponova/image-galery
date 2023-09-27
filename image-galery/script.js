c40654f1938e5e6527482f67340f9273ef9a3688
Varvara Gaponova <gaponova.varvara21091995@gmail.com>
2023-09-27 15:10:46 +0400

    feat: add getting images from api

---
+++ image-galery/script.js
@@ -0,0 +1,36 @@
+let url;
+const imgWrapper = document.querySelector('.img_wrapper');
+
+window.addEventListener("load", () => {
+    url = `https://api.unsplash.com/search/photos?query=nature&per_page=30&page=${getRandomNumber(1, 150)}&client_id=QeEezdXf5jbb0onIJwCZLOykIigLacF63HjPlPEWdmw`;
+});
+
+async function getData() {
+    url = `https://api.unsplash.com/search/photos?query=nature&per_page=30&page=${getRandomNumber(151, 334)}&client_id=QeEezdXf5jbb0onIJwCZLOykIigLacF63HjPlPEWdmw`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    showData(data);
+};
+
+function showData(data) {
+    for (let i = 0; i < 12; i++) {
+        const div = document.createElement('div');
+        div.classList.add('img_content');
+        imgWrapper.appendChild(div);
+
+        const img = document.createElement('img');
+        img.classList.add('img_unsplash');
+        img.src = data.results[i].urls.regular;
+        console.log(getRandomNumber(0, 29))
+        div.appendChild(img);
+    }
+}
+
+getData();
+
+function getRandomNumber(min, max) {
+    let minNum = Math.ceil(min);
+    let maxNum = Math.floor(max);
+    return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+};
